da56d988f213ea9d6052695c8225e80c9acb53f6
lord-aceldama <aceldama.v1.0@gmail.com>
2021-03-26 21:36:10 +0000

    :)

--- client.py
+++ client.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     parser.add_argument("host", help="The remote hostname/ip to connect to.")
     parser.add_argument(
         "-p", "--port", required=False, type=int, default=DEFAULT_PORT,
-        help="The remote port to connect to. (default: '%(default)s')"
+        help="The remote port to connect to. (default: %(default)s)"
     )
     #args = parser.parse_args()
     args = parser.parse_args(["192.168.1.5"])
