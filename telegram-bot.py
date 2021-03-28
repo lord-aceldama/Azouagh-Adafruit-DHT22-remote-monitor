345de9a1f1d998859c3ffffd919fb17d8ed8391c
lord-aceldama <aceldama.v1.0@gmail.com>
2021-03-28 02:22:42 +0100

    :)

--- telegram-bot.py
+++ telegram-bot.py
@@ -1,6 +1,6 @@
 import time
 from datetime import datetime as DateTime
-from typing import Optional
+from typing import Optional, List, Dict
 
 from telegram import Message
 from telegram.ext import Updater, MessageHandler, Filters
@@ -30,66 +30,91 @@ DHT_PIN = 4
 # Globals
 LOW, NORMAL, HIGH = (-1, 0, 1)
 message_queue = []
-last_message: Optional[Message] = None
+recipients: Dict[str, Message] = {}
 
 
 #----------------------------------------------------------------------------------------------------------------------
-def send_message(text):
-    #global message_queue
+def send_message(text, message: Optional[Message] = None):
+    """ Sends a message and keeps track of all recipients.
 
-    if last_message is None:
+    :param text: The text to send (or add to the queue).
+    :param message: The message object to reply to.
+    :return:
+    """
+    global recipients
+
+    # Register a user if one hasn't been set yet
+    if (message is not None) and (type(message) is Message):
+        recipients[message.from_user.username] = message
+
+    # Send the message
+    if len(recipients) == 0:
+        # No recipients yet so add it to the queue
         message_queue.append([DateTime.now(), text])
     else:
-        last_message.reply_text(text)
-        if len(message_queue) > 0:
-            last_message.reply_text("There were also some unread messages while you were away.")
-            while len(message_queue) > 0:
-                popped = message_queue.pop()
-                last_message.reply_text(f"{popped[0]}\n\n{popped[1]}")
-
-
-def last_received(message: Message):
-    """Keeps track of where the last message came from so we can reply to it at any time."""
-    global last_message
-    last_message = message
+        # Send it
+        for sender in recipients:
+            recipients[sender].reply_text(text)
+            if len(message_queue) > 0:
+                # Send any messages that weren't seen yet
+                recipients[sender].reply_text("There were also some unread messages while you were away.")
+                while len(message_queue) > 0:
+                    popped = message_queue.pop()
+                    recipients[sender].reply_text(f"{popped[0]:%Y/%m/%d %H%M%S}\n\n{popped[1]}")
 
 
 def command_handler(update, context):
-    """"""
-    last_received(update.message)
+    """ Handles user commands. """
+    global recipients
 
-    clean_text = update.message.text.strip().lower()
-    if clean_text in ['hi', 'hellow', 'howdee']:
+    clean_text: str = update.message.text.strip().lower()
+    if clean_text in ["/start", "hi", "hello", "howdy"]:
         # Greet the user
-        send_message("Well, howdee partner!")
+        send_message("Well, howdy partner!", update.message)
     elif clean_text in ['temp', 'rh', 'data']:
         # User requested details
         ada_rh, ada_temp = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
         if None in [ada_rh, ada_temp]:
-            send_message("The sensor is currently down.")
+            send_message("The sensor is currently down.", update.message)
         else:
             if clean_text == "temp":
-                send_message(f"The current {THRESHOLD_TEMP[2].lower()} is {ada_temp:.2f}{THRESHOLD_TEMP[3]}.")
+                send_message(
+                    f"The current {THRESHOLD_TEMP[2].lower()} is {ada_temp:.2f}{THRESHOLD_TEMP[3]}.",
+                    update.message
+                )
             if clean_text == "temp":
-                send_message(f"The current {THRESHOLD_RH[2].lower()} is {ada_rh:.2f}{THRESHOLD_RH[3]}.")
+                send_message(
+                    f"The current {THRESHOLD_RH[2].lower()} is {ada_rh:.2f}{THRESHOLD_RH[3]}.",
+                    update.message
+                )
             else:
                 send_message(
-                    f"The current sensor reading is {ada_temp:.2f}{THRESHOLD_TEMP[3]} at {ada_rh:.2f}{THRESHOLD_RH[3]}."
+                    f"The current sensor reading is {ada_temp:.2f}{THRESHOLD_TEMP[3]} at " +
+                    f"{ada_rh:.2f}{THRESHOLD_RH[3]}.",
+                    update.message
                 )
+    elif clean_text in ["forget", "shut up", "quiet", "mute"]:
+        # Mute the bot for current user
+        send_message(f"You'll no longer receive updates. Just message me if you want them again.", update.message)
+        recipients.pop(update.message.from_user.username)
     elif clean_text == "ping":
-        send_message(f"pong")
+        # Request pong
+        send_message(f"pong", update.message)
+    elif clean_text in ["list", "users", "recipients"]:
+        user_list = recipients.keys()
+        send_message(f"There are {len(user_list)} receiving updates: {', '.join(user_list)}", update.message)
     else:
-        send_message(f"I don't know what to do with '{clean_text}'.")
+        send_message(f"I don't know what '{clean_text}' means.", update.message)
 
 
 #----------------------------------------------------------------------------------------------------------------------
-def monitor_threshold(bound, value, current_status):
-    """
+def monitor_threshold(bound: List, value: float, current_status: int) -> int:
+    """ Notifies the user if a reading changed between LOW, NORMAL or HIGH.
 
-    :param bound:
-    :param value:
-    :param current_status:
-    :return:
+    :param bound: Either THRESHOLD_RH or THRESHOLD_TEMP.
+    :param value: The current value for the sensor reading.
+    :param current_status: The current status. (ie. LOW, NORMAL or HIGH)
+    :return: The new status.
     """
     result = NORMAL
     if (current_status != NORMAL) and (bound[0] <= value <= bound[1]):
@@ -116,12 +141,12 @@ def monitor_threshold(bound, value, current_status):
     return result
 
 
-def monitor_sensor(old_status, new_status):
-    """
+def monitor_sensor(old_status: bool, new_status: bool) -> bool:
+    """ Notifies the user if the sensor status changed from off-to-online or vise versa.
 
-    :param old_status:
-    :param new_status:
-    :return:
+    :param old_status: The current status of the sensor.
+    :param new_status: The new status of the sensor.
+    :return: The new status of the sensor.
     """
     if old_status != new_status:
         if new_status:
@@ -160,9 +185,11 @@ def main():
                 status_rh = monitor_threshold(THRESHOLD_RH, ada_rh, status_rh)
 
             time.sleep(5)
+
     except KeyboardInterrupt:
-        pass
-        #updater.idle()
+        print("User quit with keyboard interrupt.")
+
+    updater.stop()
 
 
 #----------------------------------------------------------------------------------------------------------------------
